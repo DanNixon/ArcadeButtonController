797ef467d537794bdd70155f16c53771113ab7fa
Dan Nixon <dan@dan-nixon.com>
2017-08-12 21:53:53 +0100

    clang-format

--- keyboard/src/main.ino
+++ keyboard/src/main.ino
@@ -9,28 +9,25 @@ struct buttonMap_t
   bool last;
 };
 
-buttonMap_t buttonMapping[] =
-{
-  {'a', 14},
-  {'d',  15},
-  {'w', 16},
-  {'s',  17},
-  {'e', 12},
-  {'q', 11},
-  {'r', 10},
-  {'f', 9},
-  {'z', 8},
-  {'x', 7},
-  {'1', 6},
-  {'2', 5},
-  {BUTTONMAP_END, BUTTONMAP_END}
-};
+buttonMap_t buttonMapping[] = {{'a', 14},
+                               {'d', 15},
+                               {'w', 16},
+                               {'s', 17},
+                               {'e', 12},
+                               {'q', 11},
+                               {'r', 10},
+                               {'f', 9},
+                               {'z', 8},
+                               {'x', 7},
+                               {'1', 6},
+                               {'2', 5},
+                               {BUTTONMAP_END, BUTTONMAP_END}};
 
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
 
-  buttonMap_t * bm = buttonMapping;
+  buttonMap_t *bm = buttonMapping;
   while (bm->key != BUTTONMAP_END && bm->pin != BUTTONMAP_END)
   {
     pinMode(bm->pin, INPUT_PULLUP);
@@ -42,7 +39,7 @@ void setup()
 
 void loop()
 {
-  buttonMap_t * bm = buttonMapping;
+  buttonMap_t *bm = buttonMapping;
   while (bm->key != BUTTONMAP_END && bm->pin != BUTTONMAP_END)
   {
     bool pressed = (digitalRead(bm->pin) == LOW);
